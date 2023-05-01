8c6c1cd91db75e207a7c6b52e7268ed4a79596e4
lyskz <lyski.zach@icloud.com>
2023-05-01 16:00:08 -0700

    add homeroutes and dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,6 +11,30 @@ router.get('/', async (req, res) => {
         );
         res.render('homepage', {
             blogPosts,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET FOR DASHBOARD
+router.get('/dashboard', async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findAll({
+            include: [
+                {
+                  model: User,
+                  attributes: ['username'],
+                },
+              ],
+        });
+        const blogPosts = blogPostData.map((bPost) => 
+        bPost.get({ plain: true })
+        );
+        res.render('dashboard', {
+            blogPosts,
+            logged_in: req.session.logged_in,
         });
     } catch (err) {
         res.status(500).json(err);
