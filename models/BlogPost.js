d5cf6ecb97896f824b1631bfd9c24872bc9484b8
lyskz <lyski.zach@icloud.com>
2023-05-01 15:14:38 -0700

    blogpost and user models done

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,2 +1,43 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class BlogPost extends Model {};
+
+BlogPost.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        creator_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+    }, 
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blogpost',
+    }
+);
+
+module.exports = BlogPost;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,12 @@
 const User = require('/User');
 const BlogPost = require('/BlogPost');
+
+User.hasMany(BlogPost, {
+    foreignKey: ''
+});
+
+BlogPost.belongsTo(User, {
+    foreignKey: ''
+});
+
+module.exports = { User, BlogPost };
