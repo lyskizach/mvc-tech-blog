9a0e6eb2dbc672ace304c8e43313fe5def4da42e
lyskz <lyski.zach@icloud.com>
2023-05-01 15:20:34 -0700

    add comment model

---
+++ models/Comment.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        creator_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('/User');
 const BlogPost = require('/BlogPost');
+const Comment = require('/Comment');
 
 User.hasMany(BlogPost, {
     foreignKey: ''
@@ -9,4 +10,4 @@ BlogPost.belongsTo(User, {
     foreignKey: ''
 });
 
-module.exports = { User, BlogPost };
+module.exports = { User, BlogPost, Comment };
