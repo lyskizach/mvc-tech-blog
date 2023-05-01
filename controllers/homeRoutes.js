a32cf818a8fce4660ef9fbe2321adaee08589dec
lyskz <lyski.zach@icloud.com>
2023-05-01 15:45:38 -0700

    add home route and utils

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,22 +1,20 @@
 const router = require('express').Router();
+const { BlogPost, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 // GET FOR HOME PAGE ROUTE
-// router.get('/', async (req, res) => {
-//   try {
-    // Get all users, sorted by name
-    // const userData = await User.findAll({
-    //   attributes: { exclude: ['password'] },
-    //   order: [['name', 'ASC']],
-    // });
-
-    // Serialize user data so templates can read it
-    // const users = userData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data into Handlebars.js template
-//     res.render('homepage', { users });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get('/', async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findAll();
+        const blogPosts = blogPostData.map((bPost) =>
+            bPost.get({ plain: true })
+        );
+        res.render('homepage', {
+            blogPosts,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
